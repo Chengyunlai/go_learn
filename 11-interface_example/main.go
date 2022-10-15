8d1e6fcc53c50d98d83b65b275c2897465219251
chengyunlai <1257997441@qq.com>
2022-10-15 09:24:09 +0800

    接口的例子

--- 11-interface_example/main.go
+++ 11-interface_example/main.go
@@ -25,6 +25,10 @@ type Cat struct {
 	Animal
 }
 
+type Eater interface {
+	eat()
+}
+
 func main() {
 	//person := Person{name: "老程", dog: Dog{name: "小黑", age: 3}, cat: Cat{name: "发财", age: 5}}
 	//fmt.Println(person) //{老程 {小黑 3} {发财 5}}
@@ -34,27 +38,52 @@ func main() {
 
 	person.feedDog(person.dog)
 	person.feedCat(person.cat)
-	person.feed(person.dog)
-	person.feed(person.cat)
+	var x Eater
+	x = person.dog
+	person.feed(x)
+	x = person.cat
+	person.feed(x)
+
+	//var x interface{}
+	//x = person.dog
+	//fmt.Println(x)
+
+}
+
+func (Cat) eat() {
+	fmt.Println("吃鱼")
+}
+
+func (Dog) eat() {
+	fmt.Println("吃骨头")
 }
 
 func (*Person) feedCat(cat Cat) {
 	fmt.Println(cat.name)
-	fmt.Println("吃东西")
+	//fmt.Println("吃东西")
+	cat.eat()
 }
 
 func (*Person) feedDog(dog Dog) {
 	fmt.Println(dog.name)
-	fmt.Println("吃东西")
+	//fmt.Println("吃东西")
+	dog.eat()
 }
 
-func (*Person) feed(Animal interface{}) {
-	switch v := Animal.(type) {
-	case Dog:
-		fmt.Println(v.name)
-		fmt.Println("吃骨头")
-	case Cat:
-		fmt.Println(v.name)
-		fmt.Println("吃鱼")
-	}
+func (*Person) feed(Animal Eater) {
+	//switch v := Animal.(type) {
+	//case Dog:
+	//	fmt.Println("--")
+	//	fmt.Println(v)
+	//	fmt.Println(v.name)
+	//	//fmt.Println("吃骨头")
+	//	v.eat()
+	//case Cat:
+	//	fmt.Println("--")
+	//	fmt.Println(v)
+	//	fmt.Println(v.name)
+	//	//fmt.Println("吃鱼")
+	//	v.eat()
+	//}
+	Animal.eat()
 }
